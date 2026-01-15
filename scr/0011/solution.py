c05f5aa360a5a17963db73ba384000bdb1ed1565
raise525 <1906093730@qq.com>
2026-01-15 11:06:22 +0800

    day 41完成啦

---
+++ scr/0011/solution.py
@@ -0,0 +1,17 @@
+from typing import List
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        i, j, res = 0, len(height) - 1, 0
+        while i < j:
+            if height[i] < height[j]:
+                res = max(res, height[i] * (j - i))
+                i += 1
+            else:
+                res = max(res, height[j] * (j - i))
+                j -= 1
+        return res
+
+# 作者：Krahets
+# 链接：https://leetcode.cn/problems/container-with-most-water/solutions/11491/container-with-most-water-shuang-zhi-zhen-fa-yi-do/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
