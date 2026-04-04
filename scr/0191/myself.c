f3e12a3cbd6c6c989b890ef63d5466bf05a9c5f4
raise525 <1906093730@qq.com>
2026-04-04 11:13:10 +0800

    day102

---
+++ scr/0191/myself.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int hammingWeight(uint32_t n){
+    int count = 0;
+    while (n)
+    {
+        count += (n & 1);
+        n >>= 1;
+    }
+    return count;
+    
+}
