1f02a5c676d9d5e51a1d109e5db0e5441476f336
raise525 <1906093730@qq.com>
2026-04-11 15:25:16 +0800

    day109

---
+++ scr/0141/myself.c
@@ -0,0 +1,25 @@
+#include <stdbool.h>
+#include <stdio.h>
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+bool hasCycle(struct ListNode *head){
+    if (head == NULL || head->next == NULL)
+        return false;
+
+    struct ListNode *slow = head;
+    struct ListNode *fast = head;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+            return true;
+    }
+
+    return false;
+}
