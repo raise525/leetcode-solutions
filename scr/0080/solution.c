87747485ee9cbf8b033c79c8204539a06fc8adbe
raise525 <1906093730@qq.com>
2026-05-18 10:34:48 +0800

    day123

---
+++ scr/0080/solution.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+int removeDuplicates(int *nums, int numsSize)
+{
+    if (numsSize <= 2)
+    {
+        return numsSize;
+    }
+    int slow = 2;
+    int fast = 2;
+    while (fast < numsSize)
+    {
+        if (nums[slow - 2] != nums[fast])
+        {
+            nums[slow] = nums[fast];
+            slow++;
+        }
+        fast++;
+    }
+    return slow;
+}
